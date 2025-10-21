b054da2d66c0d1d6b29eaf5f2513a57131c74251
MauricioTeran <mauricio.teran@utec.edu.pe>
2025-10-21 18:25:33 -0500

    codigo

---
+++ hackaton2/src/main/java/com/example/hackaton2/Hackaton2Application.java
@@ -0,0 +1,13 @@
+package com.example.hackaton2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackaton2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackaton2Application.class, args);
+	}
+
+}
